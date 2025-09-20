7535c84acafa319b0586fd56025e24303c2d815b
d1SO <ivansokotuhin1816@gmail.com>
2025-09-20 19:06:24 +0200

    Added Lab6

---
+++ 3rd Sem/Lab6/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <windows.h>
+#include <vector>
+
+
+void selSort(std::vector<int>& arr)
+{
+    int len = arr.size();
+    int steps = 0;
+    for(int i = 0; i < len; i++)
+    {
+        int min = INT_MAX;
+        int ToSwap = steps;
+        for(int j = steps; j < len; j++)
+        {
+            if (arr[j] < min)
+            {
+                min = arr[j];
+                ToSwap = j;
+            }
+        }
+        std::swap(arr[i], arr[ToSwap]);
+        steps++;
+    }
+
+    return;
+}
+
+int main()
+{
+
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+
+    std::vector<int> arr = { 5, 3, 8, 1, 2, 7 };
+    selSort(arr);
+    for (int num : arr)
+        std::cout << num << " ";
+    return 0;
+
+
+
+}
