49ce213c6245d914cac62b7ce292d54fdb0e6866
d1SO <ivansokotuhin1816@gmail.com>
2025-10-05 14:54:01 +0200

    Lab8 done

--- 3rd Sem/Lab8/main.cpp
+++ 3rd Sem/Lab8/main.cpp
@@ -8,73 +8,90 @@ std::vector<int> generateRandomVector(int length) {
     std::vector<int> vec(length);
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(-pow(10, 3), pow(10, 3));
+    std::uniform_int_distribution<> dis(-pow(10, 4), pow(10, 4));
     for (int& x : vec)
         x = dis(gen);
     return vec;
 }
 
-void RadixSort(std::vector<int> arr)
+
+void RadixSort(std::vector<int>& arr)
 {
-
-
-    std::vector<int>* bit[10];
-    std::vector<int> itBit;
-    int size = arr.size();
-    for(int i = 0; i < 10; i++)
-        bit[i] = new std::vector<int>;
-
-    int m = 0;
-    for(int i = 0; i < size; i++)
-    {
-        int k = abs(arr[i]);
-        int len = 0;
-        while(k > 0)
-        {
-            k /= 10;
-            len++;
-        }
-        m = max(m, len);
-        itBit.push_back(len);
+    std::vector<int> negatives, positives;
+    for (int x : arr) {
+        if (x < 0)
+            negatives.push_back(-x);
+        else
+            positives.push_back(x);
     }
 
+    void (*radix)(std::vector<int>&) = [](std::vector<int>& arr) {
+        std::vector<int> bit[10];
+        std::vector<int> itBit;
+        int size = arr.size();
 
-
-    for(int i = 0; i < 1; i++)
-    {
-        for(int j = 0; j < size; j++)
+        int m = 0;
+        for(int i = 0; i < size; i++)
         {
-            bit[abs(arr[j] % (int)pow(10, i+1))]->push_back(arr[j]);
-        }
-
-        for(int i = 0; i < 10; i++)
-        {
-            std::cout << i << " ";
-            for(int item : *bit[i])
+            int k = abs(arr[i]);
+            int len = 0;
+            while(k > 0)
             {
-                std::cout << item << " ";
+                k /= 10;
+                len++;
             }
-            std::cout << std::endl;
+            m = max(m, len);
+            itBit.push_back(len);
         }
-    }
 
+
+
+        for(int i = 0; i < m; i++)
+        {
+            
+            for(int j = 0; j < size; j++)
+            {
+                bit[abs((arr[j] % (int)pow(10, i+1)) / (int)pow(10, i))].push_back(arr[j]);
+            }
+
+
+            for(int c = 0; c < 10; c++)
+            {
+                std::cout << c << " ";
+                for(int item : bit[c])
+                {
+                    std::cout << item << " ";
+                }
+                std::cout << std::endl;
+            }
+
+            arr.clear();
+            std::cout << arr.size();
+
+            for(int k = 0; k < 10; k++)
+            {
+                for(int j = 0; j < bit[k].size(); j++)
+                {
+                    std::cout << "!@#123";
+                    arr.push_back(bit[k][j]);
+                }
+                bit[k].clear();
+            }
+
+            std::cout << "iter " << i << std::endl;
+        }
+
+        std::cout << "done" << std::endl;
+    };
+
+    radix(positives);
+    radix(negatives);
 
     arr.clear();
-
-
-    for(int i = 0; i < 10; i++)
-    {
-        for(int j = 0; j < bit[i]->size(); j++)
-        {
-            arr.push_back((*bit[i])[j]);
-        }
-    }
-
-    for(int i = 0; i < 10; i++)
-        std::cout << arr[i] << " ";
-
-    for(int i = 0; i < 10; i++)
-        delete bit[i];
+    for (std::vector<int>::reverse_iterator it = negatives.rbegin(); it != negatives.rend(); ++it)
+        arr.push_back(-*it);
+    for (int x : positives)
+        arr.push_back(x);
 
     return;
 }
@@ -87,14 +104,15 @@ int main()
     SetConsoleCP(65001);
 
     std::vector<int> arr = generateRandomVector(10);
+    arr.push_back(0);
 
     for(int i = 0; i < 10; i++)
         std::cout << arr[i] << " ";
     std::cout << std::endl;
     RadixSort(arr);
     
-    // for(int i = 0; i < 10; i++)
-    //     std::cout << arr[i] << " ";
+    for(int i = 0; i < 10; i++)
+         std::cout << arr[i] << " ";
     
     return 0;
 }
